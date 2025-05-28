76e538ef500c43ba3497722e5a6843226c1896fd
tranduc0602 <tranduc062lp@gmail.com>
2025-05-28 09:59:59 +0700

    &quot;Sửa Form thêm liên hệ, bổ sung trường

--- ContactList/ContactList/AddContactForm.cs
+++ ContactList/ContactList/AddContactForm.cs
@@ -16,5 +16,10 @@ namespace ContactList
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
